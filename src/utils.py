bde3b91d263ff2c678440d47585c752bba5e0994
Ayush Kashyap <ayushkashyap0109@gmail.com>
2026-06-15 11:02:47 +0530

    Added app.py And prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -17,19 +18,32 @@ def save_object(file_path, obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             
             model = list(models.values())[i]
-            model.fit(X_train,y_train) #train model
-            y_train_predict = model.predict(X_train)
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model , para ,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            # model.fit(X_train,y_train) #train model
             y_test_predict = model.predict(X_test)
-            train_model_score = r2_score(y_train, y_train_predict)
             test_model_score = r2_score(y_test, y_test_predict)
             report[list(models.keys())[i]] = test_model_score
         
         return report 
-    except:
-        pass
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
